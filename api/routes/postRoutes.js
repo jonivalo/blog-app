82e4cd839ca6a47aa8dff438da2b0131683f0d5f
jonivalo <jonivirvalo@gmail.com>
2024-01-31 06:55:35 +0200

    Add update route

--- api/routes/postRoutes.js
+++ api/routes/postRoutes.js
@@ -4,8 +4,9 @@ const postController = require('../controllers/postController');
 const authenticate = require('../middleware/authenticate');
 
 router.post('/', authenticate, postController.createPost);
-router.get('/', authenticate, postController.getAllPosts);
+router.get('/',  postController.getAllPosts);
 router.delete('/:id', authenticate, postController.deletePost);
 router.get('/:id', authenticate, postController.getPostById);
+router.put('/:id', authenticate, postController.updatePost);
 
 module.exports = router;
