cf2d4664ef98e8c6dc2ec9a1cf14748ce5c12e75
jonivalo <jonivirvalo@gmail.com>
2024-01-31 06:56:28 +0200

    Delete auth

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
-//const authenticate = require('../middleware/authenticate');
 
 router.post('/register', userController.register);
 router.post('/login', userController.login);
-//router.get('/me', authenticate, userController.getCurrentUser);
 
 module.exports = router;
