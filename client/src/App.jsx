6a999e3e4ec5a62d073ea8fba64a417630a5bcb7
jonivalo <jonivirvalo@gmail.com>
2024-01-31 06:56:55 +0200

    Home, Edit and Tag pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,8 +4,11 @@ import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import Dashboard from './pages/Dashboard';
 import Navbar from './components/Navbar';
+import Home from './pages/Home';
 import BlogPostPage from './pages/BlogPostPage';
+import EditPostPage from './pages/EditPostPage';
 import ProtectedRoute from './components/ProtectedRoute';
+import TagPage from './pages/TagPage';
 import { UserContext } from './context/UserContext';
 
 const App = () => {
@@ -15,11 +18,14 @@ const App = () => {
         <>
             <Navbar onLogout={logout} isLoggedIn={!!user} />
             <Routes>
-                <Route path="/" element={<Navigate to="/dashboard" replace />} />
+            <Route path="/" element={<Home />} />
+
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/register" element={<RegisterPage />} />
+                <Route path="/tags/:tag" element={<TagPage />} />
                 <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
                 <Route path="/posts/:postId" element={<ProtectedRoute><BlogPostPage /></ProtectedRoute>} />
+                <Route path="/edit/:postId" element={<ProtectedRoute><EditPostPage /></ProtectedRoute>} />
             </Routes>
         </>
     );
